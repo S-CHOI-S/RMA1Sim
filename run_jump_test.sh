c7e8e5bb190025937fa86940cc5f6c72ea2fe0ac
S-CHOI-S <113012648+S-CHOI-S@users.noreply.github.com>
2024-06-12 13:25:55 +0900

    Update run_jump_test.sh

--- run_jump_test.sh
+++ run_jump_test.sh
@@ -1,3 +1,3 @@
 cd raisimGymTorch/raisimGymTorch/env/envs/rsg_anymal
-python tester.py -w /home/kist/raisim_ws/raisimLib/raisimGymTorch/data/anymal_locomotion/jump/full_1800.pt
+python tester.py -w ../../../../data/anymal_locomotion/jump/full_1800.pt
 cd ../../../../../
